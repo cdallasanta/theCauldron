8ba73fc652950562e4882969821015a1f7ecfa47
Chris Dalla Santa <chris.dallasanta@gmail.com>
2018-12-17 04:07:56 +0000

    working #findLowestHP

--- functions.js
+++ functions.js
@@ -38,12 +38,28 @@ function destroyCard(owningPlayer, targetCard){
   owningPlayer.trash.push(owningPlayer.playArea.splice(targetCard));
 }
 
+//find the objects with the highest currentHP
 function findHighestHP(targets){
+  //go through each object and find the hightest HP on the board
   var highestHP = 0;
   for (var i=0;i<targets.length;i++){
     if (targets[i].currentHP > highestHP){
       highestHP = targets[i].currentHP;
     }
   }
+  //return an array with all targets whose currentHP is equal to the highest HP
   return targets.filter(function(target){return target.currentHP == highestHP});
+}
+
+//find the objects with the lowest currentHP
+function findLowestHP(targets){
+  //go through each object and find the lowest HP on the board
+  var lowestHP = 0;
+  for (var i=0;i<targets.length;i++){
+    if (targets[i].currentHP < lowestHP){
+      lowestHP = targets[i].currentHP;
+    }
+  }
+  //return an array with all targets whose currentHP is equal to the lowest HP
+  return targets.filter(function(target){return target.currentHP == lowestHP});
 }
